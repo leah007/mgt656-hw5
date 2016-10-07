7a6cd9f7bc7efb659597df56fdc1b221baa9ae87
Leah007 <leah.karlins@yale.edu>
2016-10-07 01:51:44 +0000

    Fix the first part

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656! I am a badass.');
+    response.send('Hello MGT-656!');
 });
 
 app.get('/nickname', function(request, response){
