3b311f1a7d94e1fd20ac67721b496f701bb53927
Leah007 <leah.karlins@yale.edu>
2016-10-07 01:42:11 +0000

    Added nickname response

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a badass.');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('encouraging-porcupine');
 });
 
 app.listen(process.env.PORT || 4000);
